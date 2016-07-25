1bb401bce6839a37dbfb58e75ad8481d484f8ce2
devmario <mario@vanillabreeze.com>
2016-07-26 03:29:32 +0900

    notify itunes script get property

--- bin/np_itunes.sh
+++ bin/np_itunes.sh
@@ -1,27 +1,54 @@
 #!/usr/bin/env osascript
 
-tell application "System Events"
-	set process_list to (name of every process)
-end tell
-
-if process_list contains "iTunes" then
-	tell application "iTunes"
-		set info to current stream title
-		if player state is playing then
-			if info is missing value then
-			    set info to (get name of current track)
-				if (get album of current track) is not "" then
-					set info to info & " : " & (get album of current track)
-				end if
-				if (get artist of current track) is not "" then
-					set info to info & " : " & (get artist of current track)
-				end if
-				set info to info
-			else
-				set info to (get name of current track) & " : " & info
-			end if
-		else
-			set info to "mute..."
-		end if
+on run argv
+	tell application "System Events"
+		set process_list to (name of every process)
 	end tell
-end if
+
+	if process_list contains "iTunes" then
+		tell application "iTunes"
+
+			set info to ""
+
+			if player state is playing then
+				if (item 1 of argv) is "stream" then
+					set info to current stream title
+				else if (item 1 of argv) is "count" then
+					set info to (count of tracks of current playlist)
+				else if (item 1 of argv) is "list_name" then
+					set info to (name of current playlist)
+				else if (item 1 of argv) is "index" then
+					set info to index of current track
+				else if (item 1 of argv) is "name" then
+					set info to (get name of current track)
+				else if (item 1 of argv) is "album" then
+					set info to (get album of current track)
+				else if (item 1 of argv) is "artist" then
+					set info to (get artist of current track)
+				else if (item 1 of argv) is "genre" then
+					set info to genre of current track
+				end if
+			end if
+
+			if (item 1 of argv) is "status" then
+				set info to player state
+			end if
+
+			if info is (missing value) then
+				set info to "."
+			end if
+
+			if info is "" then
+				set info to "."
+			end if
+
+			return info
+
+		end tell
+	else
+		if (item 1 of argv) is "status" then
+			set info to "iTune not running"
+			return info
+		end if
+	end if
+end run
