479976324c2d62957e1025289f11718869144273
devmario <mario@vanillabreeze.com>
2016-07-26 02:11:03 +0900

    return found video

--- api_youtube.js
+++ api_youtube.js
@@ -49,7 +49,7 @@ fetch(makeQuery(command, {q:q, type:'video'}))
             }
             const videoId = item.id.videoId;
             const title = item.snippet.title;
-            console.log(num + " : " + title);
+            console.log(num + " : " + title, "\n");
 
             const cover = `${__dirname}/tmp/jpg/${videoId}.jpg`;
             const url = item.snippet.thumbnails.high.url;
